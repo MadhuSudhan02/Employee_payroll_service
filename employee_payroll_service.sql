648f0e75d5eeafcc567061790540f0fb3f2f64fd
Madhu Sudhan <m02sudhan@gmail.com>
2022-11-17 20:00:23 +0530

    Added : creating employee payroll table

--- employee_payroll_service.sql
+++ employee_payroll_service.sql
@@ -1,3 +1,7 @@
 create database payroll_service;
 show databases;
 use payroll_service;
+ create table employee_payroll ( id INT unsigned NOT NULL AUTO_INCREMENT, name VARCHAR(150) NOT NULL, salary Double NOT NULL, start DATE NOT NULL, PRIMARY KEY (id)
+);
+select * from employee_payroll;
+
